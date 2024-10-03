758cf513f60f2f6f9daa6a943542339127674ef3
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-10-03 20:33:21 +0000

    increased timeout

--- server.py
+++ server.py
@@ -4,5 +4,5 @@ from src.api.llama_vision import LlamaVisionAPI
 
 if __name__ == "__main__":
     api = LlamaVisionAPI()
-    server = ls.LitServer(api, spec=ls.OpenAISpec())
+    server = ls.LitServer(api, spec=ls.OpenAISpec(), timeout=60)
     server.run(port=8000, generate_client_file=False)
